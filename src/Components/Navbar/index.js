c79b8c4e45a68f111503e460ae43f07f40bc6b96
Pragnan143 <pragnanmanam8@gmail.com>
2025-01-11 03:05:42 +0530

    Navbar Component Finished

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,7 +1,27 @@
 import React from "react";
 
 const index = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="h-[60px] w-full bg-purple-500 flex gap-8 justify-between items-center p-6 ">
+      <div>Pursuit Technologies</div>
+      <div className="w-[50%] flex items-center justify-between">
+        <div className="flex gap-3 justify-evenly w-[70%] font-medium text-white ">
+          <a href="#">Home</a>
+          <a href="#">Courses</a>
+          <a href="#">About Us</a>
+          <a href="#">Testinomials</a>
+        </div>
+        <div className="flex gap-6">
+          <div className="h-10 w-28 bg-white  flex items-center justify-center rounded-md">
+            <p>Login</p>
+          </div>
+          <div className="h-10 w-28 bg-white  flex items-center justify-center rounded-md">
+            <p>Sign Up</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default index;
